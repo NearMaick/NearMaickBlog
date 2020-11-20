878acb1c741ac2d3301ec4c3a3d786b0730c6691
Maick Souza <maick_a_s@msn.com>
2020-11-20 18:57:06 -0300

    feat(web): listing posts #2

--- src/_posts/lances.md
+++ src/_posts/lances.md
@@ -1,1 +1,6 @@
+---
+title: "Alguns nuances que aprendi ao longo da jornada"
+description: "Neste tópico eu coloco alguns lances que aprendi ao longo do desenvolvimento."
+---
+
 Agora sei porque em alguns casos (principalmente no Next.js) não pode usar o React.FC: porque dá conflito na tipagem com as props das funcções nativas do Next
--- src/api/postApi.ts
+++ src/api/postApi.ts
@@ -1,16 +1,17 @@
 import matter from 'gray-matter'
 import marked from 'marked'
 
-export async function getAllPosts() {
+export async function getAllPosts(): Promise<string[]> {
   const context = require.context('../_posts', false, /\.md$/)
   const posts = []
 
   for (const key of context.keys()) {
     const post = key.slice(2)
     const content = await import(`../_posts/${post}`)
-    console.log(content)
+    const meta = matter(content.default)
     posts.push({
-      slug: post.replace('.md', '')
+      slug: post.replace('.md', ''),
+      title: meta.data.title
     })
   }
 
